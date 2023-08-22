7af64aea7e5c2b9101d5f9c46b410cd2775ca5a0
nelson chege <nelson3chege@gmail.com>
2023-08-22 21:29:06 +0300

    relocate the navbar

--- number_5/bitnine/app/page.tsx
+++ number_5/bitnine/app/page.tsx
@@ -1,3 +1,11 @@
+import Navbar from "./components/NavBar";
+
 export default function Home() {
-  return <div className="mt-20 min-h-screen">hello world</div>;
+  return (
+    <div className="mt-20 min-h-screen">
+      {" "}
+      <Navbar />
+      hello world
+    </div>
+  );
 }
